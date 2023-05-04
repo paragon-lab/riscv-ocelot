a86bef482915cf497791d90790fb3e4490991513
Connor Selna <connorselna2021@u.northwestern.edu>
2023-05-04 16:20:28 -0500

    tt_vec_mul changing out from wire-> reg, all non-blocking assignments

--- src/main/resources/vsrc/rvv/tt_vec_mul_dp.sv
+++ src/main/resources/vsrc/rvv/tt_vec_mul_dp.sv
@@ -10,22 +10,55 @@ module tt_vec_mul_dp #(parameter
 		     input 		       i_mulen_0a,
 		     input i_clk ,
 		 
-		     output logic [VLEN/8-1:0][128:0] o_sum_1a
+		     output reg [VLEN/8-1:0][128:0] o_sum_1a
 		     );
    
-   always_ff @(posedge i_clk) begin
-      if (i_mulen_0a) begin
-         for(int i=VLEN/16;i<VLEN/8; i++) o_sum_1a[i][ 16:0] <= {{ 9{i_issgn_0a && i_sized_src1_0a[i][ 7]}}, i_sized_src1_0a[i][ 7:0]} * {{ 9{i_issgnsrc2_0a && i_sized_src2_0a[i][ 7]}}, i_sized_src2_0a[i][ 7:0]};
-         for(int i=VLEN/32;i<VLEN/16;i++) o_sum_1a[i][ 32:0] <= {{17{i_issgn_0a && i_sized_src1_0a[i][15]}}, i_sized_src1_0a[i][15:0]} * {{17{i_issgnsrc2_0a && i_sized_src2_0a[i][15]}}, i_sized_src2_0a[i][15:0]};
-         for(int i=VLEN/64;i<VLEN/32;i++) o_sum_1a[i][ 64:0] <= {{33{i_issgn_0a && i_sized_src1_0a[i][31]}}, i_sized_src1_0a[i][31:0]} * {{33{i_issgnsrc2_0a && i_sized_src2_0a[i][31]}}, i_sized_src2_0a[i][31:0]};
-         for(int i=0;      i<VLEN/64;i++) o_sum_1a[i][128:0] <= {{65{i_issgn_0a && i_sized_src1_0a[i][63]}}, i_sized_src1_0a[i][63:0]} * {{65{i_issgnsrc2_0a && i_sized_src2_0a[i][63]}}, i_sized_src2_0a[i][63:0]};
+ always_ff @(posedge i_clk) begin
+    if (i_mulen_0a) begin
+      for(int i=VLEN/16;i<VLEN/8; i++) begin
+        o_sum_1a[i][ 16:0] <= {{ 9{i_issgn_0a && i_sized_src1_0a[i][ 7]}},
+                                 i_sized_src1_0a[i][ 7:0]} * {{ 9{i_issgnsrc2_0a && i_sized_src2_0a[i][ 7]}},
+                                 i_sized_src2_0a[i][ 7:0]};
+        o_sum_1a[i][128:17] <= '0;
       end
-   end
+
+      for(int i=VLEN/32;i<VLEN/16;i++) begin
+        o_sum_1a[i][ 32:0] <= {{17{i_issgn_0a && i_sized_src1_0a[i][15]}},
+                                i_sized_src1_0a[i][15:0]} * {{17{i_issgnsrc2_0a && i_sized_src2_0a[i][15]}},
+                                i_sized_src2_0a[i][15:0]};
+        o_sum_1a[i][128:33] <= '0;
+      end
+
+      for(int i=VLEN/64;i<VLEN/32;i++) begin 
+        o_sum_1a[i][ 64:0] <= {{33{i_issgn_0a && i_sized_src1_0a[i][31]}}, 
+                                i_sized_src1_0a[i][31:0]} * {{33{i_issgnsrc2_0a && i_sized_src2_0a[i][31]}}, 
+                                i_sized_src2_0a[i][31:0]};
+        o_sum_1a[i][128:65] <= '0;
+      end
+
+      for(int i=0;i<VLEN/64;i++) begin 
+        o_sum_1a[i][128:0] <= {{65{i_issgn_0a && i_sized_src1_0a[i][63]}},
+                                   i_sized_src1_0a[i][63:0]} * {{65{i_issgnsrc2_0a && i_sized_src2_0a[i][63]}},
+                                   i_sized_src2_0a[i][63:0]};
+      end
+
+    end
+ end
+
+ //to fix compile errors regarding mixed blocking and non-blocking
+ //assignments for o_sum_1a:
+ //-comment out old always_comb block
+ //-create new zeroes wire
+ //-sequentially assign in always_ff block
+ //-pray for forgiveness
+//` wire [128:0] zeroes = 129'b0; 
    
-   always_comb begin
-      for(int i=VLEN/16;i<VLEN/8; i++) o_sum_1a[i][128:17] = '0;
-      for(int i=VLEN/32;i<VLEN/16;i++) o_sum_1a[i][128:33] = '0;
-      for(int i=VLEN/64;i<VLEN/32;i++) o_sum_1a[i][128:65] = '0;
-   end
+ //high-key take a look at these indices later--why starting on 1th bit of
+ //a new word?
+// always_comb begin
+//    for(int i=VLEN/16;i<VLEN/8; i++) o_sum_1a[i][128:17] = '0;
+//    for(int i=VLEN/32;i<VLEN/16;i++) o_sum_1a[i][128:33] = '0;
+//    for(int i=VLEN/64;i<VLEN/32;i++) o_sum_1a[i][128:65] = '0;
+// end
    
 endmodule
